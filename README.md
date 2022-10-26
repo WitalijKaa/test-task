91a03e28c83f654e3e9a4bf53c16fa9e72715a20
Witalij Kaa <witalijkaa@hotmail.com>
2022-10-26 16:19:15 +0200

    murder

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-# Yet Another Framework WTF
-
-## just play:
-
-### see what is it
-### see what is it
+# MURDER
--- application/actions/main/PostAction.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class PostAction extends Yaf_Action_Abstract {
-
-    public function execute() {
-        $this->getView()->appName = "magic-stone-circuit.app";
-    }
-}
