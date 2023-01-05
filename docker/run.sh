a282e8ce88cb0b40821d6909b3fec217bfc89646
Witalij Kaa <witalijkaa@hotmail.com>
2023-01-05 15:36:47 +0100

    debian 11 upd

--- docker/run.sh
+++ docker/run.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+rm /var/www/storage/logs/laravel.log
 cd /var/www
 
 php artisan migrate:fresh --seed
 php artisan cache:clear
 php artisan route:cache
+
+chmod 774 $(find /var/www/storage/ -type d)
+chmod 664 $(find /var/www/storage/ -type f)
+rm /var/www/storage/logs/laravel.log
 
 /usr/bin/supervisord -c /etc/supervisord.conf
 
